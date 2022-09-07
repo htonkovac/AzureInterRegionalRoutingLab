d722ac6803035c9afd17a1c766c7c25c8cdd70d0
Hrvoje Tonkovac <hrvoje.tonkovac@toptal.com>
2022-09-07 23:59:34 +0200

    forgot to commit bastion module

---
+++ terraform-modules/bastion/main.tf
@@ -0,0 +1,20 @@
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.name}-pip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = var.subnet_id
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
